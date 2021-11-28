99beb4318664380bd7f7b394a1c146cd142edba1
Christoph Stiller <cstiller@euclideonholographics.com>
2021-11-28 14:55:52 +0100

    How about this?

--- llscript_exec/src/main.c
+++ llscript_exec/src/main.c
@@ -6,80 +6,6 @@
 
 #include "llshost.h"
 
-#ifdef _WIN32
-#include <inttypes.h>
-#include <Windows.h>
-#include <winnt.h>
-#include <DbgHelp.h>
-
-#pragma comment(lib, "Dbghelp.lib")
-
-LONG WINAPI TopLevelExceptionHandler(IN EXCEPTION_POINTERS *pExceptionInfo)
-{
-  printf("Exception triggered: 0x%" PRIX32 ".\n", pExceptionInfo->ExceptionRecord->ExceptionCode);
-
-  do
-  {
-    char filename[MAX_PATH] = "llscript_exec.exe"; // Already filled in case `GetModuleFileNameA` fails.
-    GetModuleFileNameA(GetCurrentProcess(), filename, sizeof(filename));
-
-    const char dmpFileExtension[] = ".dmp";
-    strncat_s(filename, sizeof(filename), dmpFileExtension, sizeof(dmpFileExtension));
-
-    HANDLE fileHandle = CreateFileA(filename, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-    if (fileHandle == INVALID_HANDLE_VALUE)
-      break;
-
-    MINIDUMP_EXCEPTION_INFORMATION exceptionInfo;
-    memset(&exceptionInfo, 0, sizeof(exceptionInfo));
-
-    exceptionInfo.ThreadId = exceptionInfo.ThreadId;
-    exceptionInfo.ExceptionPointers = exceptionInfo.ExceptionPointers;
-    exceptionInfo.ClientPointers = TRUE;
-
-    printf("Attempting to write minidump to '%s'.", filename);
-
-    if (TRUE != MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), fileHandle, MiniDumpWithFullMemory | MiniDumpWithThreadInfo, &exceptionInfo, NULL, NULL))
-      printf("Failed to write crash dump with error code 0x%" PRIu32 ".\n", GetLastError());
-
-  } while (0);
-
-  fflush(stdout);
-
-  return EXCEPTION_CONTINUE_SEARCH;
-}
-
-BOOL WINAPI SignalHandler(DWORD type)
-{
-  printf("Signal triggered: 0x%" PRIX32 ".\n", type);
-
-  do
-  {
-    char filename[MAX_PATH] = "llscript_exec.exe"; // Already filled in case `GetModuleFileNameA` fails.
-    GetModuleFileNameA(GetCurrentProcess(), filename, sizeof(filename));
-
-    const char dmpFileExtension[] = ".dmp";
-    strncat_s(filename, sizeof(filename), dmpFileExtension, sizeof(dmpFileExtension));
-
-    HANDLE fileHandle = CreateFileA(filename, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-    if (fileHandle == INVALID_HANDLE_VALUE)
-      break;
-
-    printf("Attempting to write minidump to '%s'.", filename);
-
-    if (TRUE != MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), fileHandle, MiniDumpWithFullMemory | MiniDumpWithThreadInfo, NULL, NULL, NULL))
-      printf("Failed to write crash dump with error code 0x%" PRIu32 ".\n", GetLastError());
-
-  } while (0);
-
-  fflush(stdout);
-
-  return TRUE;
-}
-#endif
-
 int32_t main(const int32_t argc, const char **pArgv)
 {
   if (argc != 2)
@@ -88,40 +14,6 @@ int32_t main(const int32_t argc, const char **pArgv)
     puts("Build Time: " __TIMESTAMP__);
     return -1;
   }
-
-#ifdef _WIN32
-  CoInitialize(NULL);
-  SetUnhandledExceptionFilter(TopLevelExceptionHandler);
-  SetConsoleCtrlHandler(SignalHandler, TRUE);
-
-  // Prevent anyone in the future to override our unhandled exception filter.
-  do
-  {
-    HANDLE kernel32 = LoadLibraryA("kernel32.dll");
-
-    if (kernel32 == INVALID_HANDLE_VALUE || kernel32 == NULL)
-      break;
-
-     void *pPosition = (void *)GetProcAddress(kernel32, "SetUnhandledExceptionFilter");
-    
-    if (pPosition == NULL)
-      break;
-
-    const uint8_t shellcode[] = { 0x33, 0xC0, 0xC2, 0x04, 0x00 }; // xor eax, eax; ret 0x4; -> return 0;
-    DWORD oldProtect;
-    
-    if (FALSE == VirtualProtect(pPosition, sizeof(shellcode), PAGE_EXECUTE_READWRITE, &oldProtect))
-      break;
-    
-    if (FALSE == WriteProcessMemory(GetCurrentProcess(), pPosition, shellcode, sizeof(shellcode), NULL))
-      break;
-
-    if (FALSE == VirtualProtect(pPosition, sizeof(shellcode), oldProtect, NULL))
-      break;
-
-    FreeLibrary(kernel32);
-  } while (0);
-#endif
 
   uint8_t *pByteCode = NULL;
 
